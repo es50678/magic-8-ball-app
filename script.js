a0acb768e384482df97d5d4d08e3659c0dd4d1b2
es50678 <es50678@gmail.com>
2017-10-15 22:34:01 -0400

    changes picture and fades in text based on user input

--- script.js
+++ script.js
@@ -10,15 +10,22 @@ var MagicEightBall = {};
             "Most likely"
         ];
             
-    MagicEightBall.getAnswer = function(q){
+    MagicEightBall.getAnswer = function(){
+        $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
         var randomNumber = Math.floor(Math.random() * this.answerList.length);
         answer = this.answerList[randomNumber];
         $("#answer").text(answer);
+        $("#answer").fadeIn(8000);
     };
 
+$("#answer").hide();
 var showPrompt = function show_prompt() {
-    var question = prompt("Ask Me Anything");
-    MagicEightBall.getAnswer(question);
+    $("#answer").hide();
+    $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
+    setTimeout(function(){
+        prompt("ask me a yes or no question");
+    }, 500);
+    MagicEightBall.getAnswer();   
 }
 
 $('#questionButton').click(showPrompt);
