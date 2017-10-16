d1d5f69db86a9ba76a02a763ae080cd40078cf3b
es50678 <es50678@gmail.com>
2017-10-16 10:47:13 -0400

    implements shaking ball

--- script.js
+++ script.js
@@ -11,11 +11,17 @@ var MagicEightBall = {};
         ];
             
     MagicEightBall.getAnswer = function(){
-        $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+        setTimeout(function(){
+            $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+        }, 500);
         var randomNumber = Math.floor(Math.random() * this.answerList.length);
         answer = this.answerList[randomNumber];
+        setTimeout(function(){
+            $("#8ball").effect("shake");
+        }, 500);
         $("#answer").text(answer);
         $("#answer").fadeIn(8000);
+        
     };
 
 $("#answer").hide();
